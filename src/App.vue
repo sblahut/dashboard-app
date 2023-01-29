bf471502aa37b60e66a39fac8589a1e8a9822f77
Steven Blahut <sblahut@Stevens-MBP.hsd1.va.comcast.net>
2023-01-29 13:11:46 -0500

    added LineChart & updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
+<template>
+  <div id="app" class="container">
+    <div class="row mt-5" v-if="arrPositive.length > 0">
+      <div class="col">
+        <h2>Positive</h2>
+        <line-chart 
+        :chartData="arrPositive" 
+        :options="chartOptions" 
+        label="Postive">
+        </line-chart>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
 <script>
-import axious from 'axios'; 
+import axios from "axios"; 
+import moment from "moment";
+
+import LineChart from "./components/LineChart";
+
 export default {
-  name: 'App',
-  components: {
-    
-  },
+  name: "app",
+  components: { LineChart },
+  data() {
+      return {
+        arrDate: [],
+        arrState: [],
+        arrPositive: [],
+        arrNegative: [],
+        chartOptions: {
+          responsive: true,
+          maintainAspectRatio: false
+      }
+    };
+},
+async created() {
+  const { data } = await axios.get("https://api.covidtracking.com/v1/us/current.json");
+  
+  data.forEach(d => {
+    const date = moment(d.date, "YYYYMMDD").format("MM/DD");
+
+    const {
+      state,
+      positive,
+      negative
+      
+    } = d;
+
+    this.arrState.push({date, total: state});
+    this.arrPositive.push({date, total: positive});
+    this.arrNegative.push({date, total: negative});
+
+    console.log(this.arrPositive);
+  })
+}
 }
 </script>
 
-<template>
-  <header>
 
-    <div class="wrapper">
-
-    </div>
-  </header>
-
-  <main>
-
-  </main>
-</template>
-
-<style scoped>
-
-</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,36 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+    extends: Line,
+    props: {
+        label: {
+            type: String
+        },
+        chartData: {
+            type: Array
+        },
+        options: {
+            type: Object
+        }
+    },
+    mounted() {
+        // to pass properties with chart.js must be flat arrays/1 dimensional
+        const dates = this.chartData.map(d => d.data).reverse();
+        const totals = this.chartData.map(d => d.total).reverse();
+
+        this.renderChart(
+            {
+                labels: dates,
+                datasets: [
+                    {
+                        label: this.label,
+                        data: totals
+                    }
+                ]
+            },
+            this.options
+        );
+    }
+};
+</script>
